10cadbdc5bb79ae2532774fdc1471965934a835c
teejay <tjayarancinaofficial@gmail.com>
2026-01-22 12:03:15 +0800

    feat(utils): add movie detail link to movie cards

--- resources/js/utils/renderMovies.js
+++ resources/js/utils/renderMovies.js
@@ -1,14 +1,30 @@
+import { slugify } from "./slug";
+
 const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
 
+/**
+ * Renders movie cards into a container element.
+ *
+ * @param {HTMLElement} movieCardContainer
+ * The container element where movie cards will be inserted.
+ *
+ * @param {Array<Object>} movies
+ * Array of movie objects
+ */
 export function renderMovies(movieCardContainer, movies) {
   movieCardContainer.innerHTML = '';
 
   movies.forEach((movie) => {
+    const slugTitle = slugify(movie.title);
+    const slugURL = `/movie/${movie.id}-${slugTitle}`;
+    
     let movieCard = document.createElement('div');
     movieCard.classList.add('movie-card', 'w-[200px]', 'shrink-0');
 
     movieCard.innerHTML = `
-      <img src="${IMAGE_URL}${movie.poster_path}" width="100%" alt="${movie.title}" loading="lazy">
+      <a href="${slugURL}" class="cursor-pointer">
+        <img src="${IMAGE_URL}${movie.poster_path}" width="100%" alt="${movie.title}" loading="lazy">
+      </a>
       <h3 class='mt-2'>${movie.title}</h3>
       <p>⭐ ${movie.vote_average.toFixed(1)}</p>
       <p>${movie.release_date}</p>
